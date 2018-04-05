92d320960cb2f3c7f352f54555a7bc6b020e712b
jaredag <jaredg-93@hotmail.com>
2018-04-05 14:52:12 -0600

    Starting to setup the session page

--- src/containers/Session.js
+++ src/containers/Session.js
@@ -1,10 +1,19 @@
 import React, {Component} from 'react';
+import Aux from '../hoc/Auxx';
+import Graph from '../components/Graph';
+import Portfolio from '../components/Portfolio';
+import SearchBar from '../components/SearchBar';
+import SessionSettings from '../components/SessionSettings';
+import Stocks from '../components/Stocks';
+import Tools from '../components/Tools';
 
 class Session extends Component {
 
     render() {
         return (
-            <div>Session</div>
+           <Aux>
+               
+           </Aux>
         );
     }
 }
