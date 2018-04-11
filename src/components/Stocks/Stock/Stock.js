5fbb49ecdbddb9b00ceb5ba9c16451aa5f5d06ec
jaredag <jaredg-93@hotmail.com>
2018-04-11 01:10:03 -0600

    The stocks list css

--- src/components/Stocks/Stock/Stock.js
+++ src/components/Stocks/Stock/Stock.js
@@ -1,16 +1,21 @@
 import React from 'react';
+import classes from './Stock.css';
 
 const stock = (props) => {
 
     let percentage = 100 * props.percentage;
-
+    let percentage_style = classes.PercentagePos;
+    if (percentage < 0){
+        percentage_style = classes.PercentageNeg;
+    }
+    let symbol_upper = props.symbol.toUpperCase();
     return (
-        <div>
+        <div className={classes.StockButton}>
             <button onClick={props.display}>
-                <div>{props.symbol}</div>
-                <div>{props.market_price}</div>
-                <div>{percentage}%</div>
-                <div>{props.change}</div>
+                <p className={classes.Symbol}>{symbol_upper}</p>
+                <p className={percentage_style}>{percentage}%</p>
+                <p className={classes.MarketPrice}>{props.market_price}</p>
+                <p className={classes.Change}>{props.change}</p>
             </button>
         </div>
     );
