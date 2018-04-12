f7371aad6b3230abda789b5f895d10bd91d50eed
jaredag <jaredg-93@hotmail.com>
2018-04-12 15:52:09 -0600

    Portfolio boxes were added

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -6,11 +6,26 @@ const portfolio = () => {
         <div className={classes.box}>
             <div className={classes.title}>STOCK BOX</div>
             <div className={classes.wrapper}>
-                <div className={classes.label}>Portfolio</div>
-                <div className={classes.label}>Buy Power</div>
-                <div className={classes.label}>Stock Net</div>
-                <div className={classes.label}>Returns</div>
-                <div className={classes.label}>Gains/Loss</div>
+                <div className={classes.label}>
+                    Portfolio
+                    <div className={classes.values}>950</div>
+                </div>
+                <div className={classes.label}>
+                    Buy Power
+                    <div className={classes.values}>600</div>
+                </div>
+                <div className={classes.label}>
+                    Stock Net
+                    <div className={classes.values}>350</div>
+                </div>
+                <div className={classes.label}>
+                    Returns
+                    <div className={classes.values}>-5%</div>
+                </div>
+                <div className={classes.label}>
+                    Gains/Loss
+                    <div className={classes.values}>-50</div>
+                </div>
             </div>
         </div>
     );
