70e05d7646a4a25beabb89d0265b51d4f50abc75
Jonah Green <gre11017@byui.edu>
2018-04-12 11:03:42 -0600

    Changed portfolio

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -3,7 +3,16 @@ import classes from './Portfolio.css';
 
 const portfolio = () => {
     return (
-        <div className={classes.box}>portfolio</div>
+        <div className={classes.box}>
+            <div className={classes.gridcontainer}>
+                <div className={classes.title}>STOCK BOX</div>
+                <div className={classes.port}>Portfolio</div>
+                <div className={classes.buy}>Buy Power</div>
+                <div className={classes.net}>Stock Net</div>
+                <div className={classes.returns}>Returns</div>
+                <div className={classes.gains}>Gains/Loss</div>
+            </div>
+        </div>
     );
 };
 
