85983f111732c334a61a1062e0cac1afc42e5114
Jonah Green <gre11017@byui.edu>
2018-05-10 17:27:03 -0600

    Formated Session Options and Sesssion Settings

--- src/components/SessionControls/IntervalControl/IntervalControl.js
+++ src/components/SessionControls/IntervalControl/IntervalControl.js
@@ -5,13 +5,14 @@ const intervalControl = (props) => {
     return (
         <div>
         <div className={classes.intervalTitle}>Interval</div> 
-            <select className={classes.custom} name="Interval">
+            <select className={classes.intervalSelect} name="Interval">
                 <option value="Hour">Hour</option>
                 <option value="Day">Day</option>
                 <option value="Week">Week</option>
                 <option value="Month">Month</option>
                 <option value="Finish">Finish</option>
             </select>
+            <img id="arrow" src="../../../public/arrow.png" alt="->"/>
             <button className={classes.nextButton}>-></button>
         </div>
     );
